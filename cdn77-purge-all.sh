66d2a48d539cbea61d872cb20866c6d3d5ed0ca7
Stefan Beyer <sbeyer@threatint.eu>
2024-01-24 13:58:25 +0200

    improved error handling

--- cdn77-purge-all.sh
+++ cdn77-purge-all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-if [ -z $1 ]
-then
+if [ -z $1 ] ; then
 	echo "please provide parameter <site>, e.g. www.mysite.com"
 	exit 250
 fi
 
 if [ -z "$CDN77_TOKEN" ] ; then
-        echo "please provide environment variable CDN_TOKEN, see https://client.cdn77.com/account/api"
+        echo "please provide environment variable CDN77_TOKEN, see https://client.cdn77.com/account/api"
         exit 251
 fi
 
+h=$(curl -o /dev/null -w "%{http_code}" -s --request GET https://api.cdn77.com/v3/cdn --header "Authorization: Bearer $CDN77_TOKEN")
+if [ $h != 200 ]; then
+	curl -s --request GET https://api.cdn77.com/v3/cdn --header "Authorization: Bearer $CDN77_TOKEN"
+	exit 255
+fi
+
 site_id=$(curl -s --request GET https://api.cdn77.com/v3/cdn --header "Authorization: Bearer $CDN77_TOKEN" | jq --arg site $1 '.[] | select(.cnames[] | .cname==$site) | .id')
-if [ -z $site_id ]
-then
+if [ -z $site_id ]; then
         echo "$1 is not on CDN77"
         exit 240
 fi
