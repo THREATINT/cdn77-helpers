16290a661a9e19c894730508c8e873b19deab55b
Stefan Beyer <sbeyer@threatint.eu>
2024-02-13 21:06:35 +0100

    enhanced logic

--- cdn77-prefetch.sh
+++ cdn77-prefetch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+FILE="/tmp/"$(date +"%Y-%m-%d-%Y-%H-%M")"-sitemap"
+
+function cleanup
+{
+        rm -f $FILE*
+}
+
 if [ -z $1 ] ; then
 	echo "please provide parameter <site>, e.g. www.mysite.com"
 	exit 250
@@ -15,11 +23,28 @@ if [ -z $site_id ] ; then
         exit 240
 fi
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null https://$1/sitemap.txt)
+
+status_code=$(curl --write-out %{http_code} --silent --output $FILE https://$1/sitemap.txt)
 
 if [ "$status_code" -ne 200 ] ; then
 	echo "error $status_code fetching sitemap.txt from $1"
+	cleanup
 	exit 230
 fi
 
-curl -s -X POST https://api.cdn77.com/v3/cdn/$site_id/job/prefetch -H "Content-Type: application/json" --header "Authorization: Bearer $CDN77_TOKEN" -d "$(echo "{ \"paths\":  $(curl -s -X GET https://$1/sitemap.txt | sed -e "s,^https://$1,," | jq -R -s -c 'split("\n")[:-1]'), \"upstream_host\": \"$1\" }")" | jq
+split -l 1999 $FILE $FILE.
+
+let i=$(find /tmp -type f 2> /dev/null | grep $FILE. | wc -l)
+if [ $i -ge 20 ]; then
+	let t=15
+else
+	let t=1
+fi
+
+while read f; do
+	curl -s -X POST https://api.cdn77.com/v3/cdn/$site_id/job/prefetch -H "Content-Type: application/json" --header "Authorization: Bearer $CDN77_TOKEN" -d "$(echo "{ \"paths\":  $(cat $f | sed -e "s,^https://$1,," | jq -R -s -c 'split("\n")[:-1]'), \"upstream_host\": \"$1\" }")" | jq
+	echo "Sleeping "$t"s"
+	sleep $t
+done < <(find /tmp -type f 2> /dev/null | grep $FILE.)
+
+cleanup
